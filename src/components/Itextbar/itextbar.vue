0931ba9542d9bcd686d472793038b89618d9950b
BigOG <592838715@qq.com>
2017-03-08 10:03:23 +0800

    add itext

--- src/components/Itextbar/itextbar.vue
+++ src/components/Itextbar/itextbar.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="body">
+	<div class="body" >
 	<!-- 画布主体 -->
-		<div class="canvas" style="width: 500px;height: 400px;"> <canvas id="can"></canvas></div>
+		<div class="canvas" style="width: 500px;height: 400px;"> <canvas id="can" width="500px" height="400px" ></canvas></div>
 	  
 		
 
@@ -15,14 +15,14 @@
 			</div>
 
 		<!--更改文字大小  -->
-			<div class="text_bar_item">
+			<!-- <div class="text_bar_item">
 				<select @change="optionchange"  id="text_size">
 					<option v-for="option in options_size" :value="option.value">{{ option.text }}</option>					
 				</select>
-			</div>
+			</div> -->
 
 		<!--更改文字颜色  -->
-			<div class="text_bar_item"></div>
+			<div class="text_bar_item"><button>颜色</button></div>
 
 		<!-- 更改文字的排版 -->
 			<div class="text_bar_item">
@@ -43,16 +43,13 @@
 
 <style type="text/css">
 	.canvas{
-		
-		z-index:999; 
 		position: relative;
-		margin:0px  auto;
-		border: 1px solid black;
+		margin: 0px auto;
+		
 	}
 
 	#can{
-		width: 400px;
-		height: 300px;
+		
 		border: 1px solid black;	
 	}
 
@@ -71,54 +68,59 @@
 				see:false,
 				options_style:[{text:'黑体' , value:'SimHei '},{text:'宋体' , value:'SimSun'},{text:'新宋体',value:'NSimSun'},{text:'仿宋',value:'FangSong'},{text:'楷体',value:'KaiTi'}],
 
-				options_size:[{text:'6',value:'6'},{text:'8',value:'8'},{text:'10',value:'10'},{text:'12',value:'12'},{text:'16',value:'16'},{text:'24' ,value:'24'},{text:'32',value:'32'},{text:'72' , value:'72'},{text:'100',value:'100'},{text:'200',value:'200'},{text:'240',value:'240'},{text:'480',value:'480'},],
+				// options_size:[{text:'6',value:'6'},{text:'8',value:'8'},{text:'10',value:'10'},{text:'12',value:'12'},{text:'16',value:'16'},{text:'24' ,value:'24'},{text:'32',value:'32'},{text:'72' , value:'72'},{text:'100',value:'100'},{text:'200',value:'200'},{text:'240',value:'240'},{text:'480',value:'480'},],
 
 				options_paiban:[{text:'居左',value:'left'},{text:'居中',value:'center'},{text:'居右',value:'right'},],
 			}
 		},
-
-		// ready: function(){
-			
-		// },
+		//加载函数
+		mounted:function(){
+			canvasTag = new fabric.Canvas('can');	
+		},
 
 		methods:{
+		
 			//生成文字画布
 			creatIText:function(){
-				var can = new fabric.Canvas('can');
+				if(textevent != null)
+					textevent.set({stroke: '#fff',strokeWidth: 0});	
 				this.see = false;
 				textevent=this;
-				console.log(can);
+			
 				let text = new fabric.IText('请输入文字',{
 					left:50,
 					top:50,
+				    
+ 				    
 				});
 
 				text.on('selected' , function(){
+					//这里把全局的变量变成了这个vue的主体
 					textevent.see = true;
 					textevent = this;
-
-					
-
+					textevent.set({stroke: '#ff1318',strokeWidth: 1});
 				});
 
-				can.add(text);
+				canvasTag.add(text);
 			},
 
 			//选择事件，判断选择项的响应对象
 			optionchange:function(event){
 				let ev  = event.target;
 				let tid  = ev.getAttribute('id');
-				
+				textevent.set({stroke: '#ff1318',strokeWidth: 1});
 				if(tid == "text_style"){
 					textevent.set({fontFamily : ev.value});
-
+					canvasTag.renderAll();
 				} 
-				else if(tid == "text_size"){
-					textevent.set({fontSize : ev.value});
-				}
+				// else if(tid == "text_size"){
+				// 	textevent.set({fontSize : ev.value});
+				// }
 				else if(tid == "text_paiban"){
 					textevent.set({textAlign : ev.value});
+					canvasTag.renderAll();
 				}
+				textevent.set({stroke: '#fff',strokeWidth: 0});
 
 			}
 		}
